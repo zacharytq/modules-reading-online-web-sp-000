aa4c0bf265e50160e37f1b3c111d56f3ac8a63aa
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 00:54:55 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,8 +2,8 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance
-  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
 
   attr_accessor :name
 
